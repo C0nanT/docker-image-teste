cee3a26d382731bb8aa2f19fa7576a60e68401c9
Conan Torres <conantorreswf@gmail.com>
2025-03-12 20:11:22 -0300

    fix: corrige MESSAGE env

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const messages = process.env.MESSAGES || "Hello World!";
+const message = process.env.MESSAGE || "Hello World!";
 
 app.get("/", (req, res) => {
-  res.send(messages);
+  res.send(message);
 });
 
 app.listen(port, () => {
